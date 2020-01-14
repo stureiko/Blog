822b367ea62a9938b2585b350c56ea73f5e0ac5f
Igor Stureiko <stureiko@mail.ru>
2020-01-14 18:18:46 +0300

    Добавлена структура БД и базовые шаблоны для index.html

--- fitness/views.py
+++ fitness/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 
 def hello(request):
-    n = 'Test name'
-    return render(request, 'fitness/index.html', context={'name': n})
+    n = ['Test name', 'next name']
+    left_panel_content = 'Текс левой панели'
+    return render(request, 'fitness/index.html', context={'names': n, 'left_panel': left_panel_content})
